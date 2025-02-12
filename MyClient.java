db774f342487ae4c0652c3e6adcb634204ef6516
tanuj15-b <tanuj15.b@gmail.com>
2025-02-12 21:25:13 +0530

    Add files via upload

---
+++ MyClient.java
@@ -0,0 +1,15 @@
+import Java.io.*;  
+import Java.net.*;  
+public class MyClient {  
+public static void main(String[] args) {  
+try{      
+Socket s=new Socket("localhost",6666);  
+DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+dout.writeUTF("Hello Server");  
+dout.flush();  
+dout.close();  
+s.close();  
+}catch(Exception e){System.out.println(e);}  
+}  
+}
+
